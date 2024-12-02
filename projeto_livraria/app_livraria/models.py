f31ddb68ed1f8825331ce61666a3bea86757b9b0
HelenaPetruz <helena.petruz@gmail.com>
2024-12-02 10:27:01 -0300

    entidade livros

--- projeto_livraria/app_livraria/models.py
+++ projeto_livraria/app_livraria/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class Livro(models.Model):
     titulo = models.CharField(max_length=200)
     genero = models.CharField(max_length=200)
-    sinopse = models.TextField(max_length=1000)
+    sinopse = models.TextField(max_length=4000)
+    capa = models.ImageField(upload_to='capas_livros/', null=True, blank=True)
     
     def __str__(self):
         return self.titulo
--- projeto_livraria/projeto_livraria/urls.py
+++ projeto_livraria/projeto_livraria/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from app_livraria import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #link inicial:
     path('', views.home, name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
