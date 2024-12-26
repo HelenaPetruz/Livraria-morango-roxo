53af6d7bfcd12f4a1bfdfabdbe2baa40635a0cd5
fleabagreact <luizadomato16@gmail.com>
2024-12-26 16:41:35 -0300

    mudar cor de botões

--- projeto_livraria/projeto_livraria/settings.py
+++ projeto_livraria/projeto_livraria/settings.py
@@ -118,9 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'app_livraria/static'
-]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
